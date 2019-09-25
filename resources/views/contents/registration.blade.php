f1ebb404178511437ff690e3374c0266b117bd93
Akbar Iskandar <m.akbar.iskandar@gmail.com>
2019-09-25 18:25:07 +0700

    add registration page

--- resources/views/contents/registration.blade.php
+++ resources/views/contents/registration.blade.php
@@ -22,8 +22,8 @@
                 <input type="password" name="password" id="password" class="form-control">
               </div>
               <div class="form-group">
-                <label for="email">Email</label>
-                <input type="text" name="email" id="email" class="form-control">
+                <label for="email">Name</label>
+                <input type="text" name="name" id="name" class="form-control">
               </div>
               <button class="btn btn-block btn-primary">Masuk</button>
             </form>
