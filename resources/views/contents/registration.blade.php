13cb101bfde457b77dfa85199af82d78e6babe57
Akbar Iskandar <m.akbar.iskandar@gmail.com>
2019-09-25 18:23:41 +0700

    add registration page

---
+++ resources/views/contents/registration.blade.php
@@ -0,0 +1,35 @@
+@extends('layouts.app')
+@section('main')
+  <div class="container-fluid">
+    <div class="row mt-5 justify-content-center">
+      <div class="col-3">
+        <div class="card">
+          <div class="card-header text-center">
+            <h4>Daftar Akun</h4>
+          </div>
+          <div class="card-body">
+            <form action="" method="POST">
+              <div class="form-group">
+                <label for="email">Email</label>
+                <input type="text" name="email" id="email" class="form-control">
+              </div>
+              <div class="form-group">
+                <label for="username">Username</label>
+                <input type="text" name="username" id="username" class="form-control">
+              </div>
+              <div class="form-group">
+                <label for="password">Password</label>
+                <input type="password" name="password" id="password" class="form-control">
+              </div>
+              <div class="form-group">
+                <label for="email">Email</label>
+                <input type="text" name="email" id="email" class="form-control">
+              </div>
+              <button class="btn btn-block btn-primary">Masuk</button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+@endsection
